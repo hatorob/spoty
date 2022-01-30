f4e04f66a561afa221cd53b8baa910c52e890ee7
Hector Toro <alejotoro94@hotmail.com>
2022-01-30 10:46:38 -0500

    * Se centralizo la peticiones hacia spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,28 +13,39 @@ export class SpotifyService {
     console.log("Servicio listo para usar....")
   }
 
-  getNewReleases() {
+  getQuery( query: string ){
+
+    const url = `https://api.spotify.com/v1/${ query }`;
+
     let token = "BQBeBLkxcco0ADzzlzjxEI-MWNttCJ6X7wRtgEHB2Mb38cIUY8t0zc3YTF9jgK_qHsHQWTZZg2ap0H1d8lY";
+
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers } )
-                .pipe( map( (data: any ) => {
-                  return data['albums'].items;
-                }));
-        
+
+    return this.http.get(url, { headers });
+
+  }
+
+
+  getNewReleases() {
+    /* let token = "BQBeBLkxcco0ADzzlzjxEI-MWNttCJ6X7wRtgEHB2Mb38cIUY8t0zc3YTF9jgK_qHsHQWTZZg2ap0H1d8lY";
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    }); */   // Aquí centralizamos la información para que solo funcione con getQuery y no duplicar código
+
+    return this.getQuery('browse/new-releases')
+                .pipe( map( (data: any ) => data['albums'].items));
 
   }
 
   getArtista( termino: string) {
-    let token = "BQBeBLkxcco0ADzzlzjxEI-MWNttCJ6X7wRtgEHB2Mb38cIUY8t0zc3YTF9jgK_qHsHQWTZZg2ap0H1d8lY";
+    /* let token = "BQBeBLkxcco0ADzzlzjxEI-MWNttCJ6X7wRtgEHB2Mb38cIUY8t0zc3YTF9jgK_qHsHQWTZZg2ap0H1d8lY";
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
-    });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers } )
-                    .pipe( map( (data: any) => {
-                      return data['artists'].items;
-                    }));
+    }); */
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+                .pipe( map( (data: any) => data['artists'].items));
         
   }
 }
